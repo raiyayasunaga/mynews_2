1e723e4a88e7d4118b3ec3d4c0b138a2b027fef9
頼矢 <braiya.asuket@icloud.com>
2021-04-26 16:04:20 +0900

    18章編集履歴を実装しよう

--- app/Http/Controllers/Admin/Profilecontroller.php
+++ app/Http/Controllers/Admin/Profilecontroller.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\Profile;
+use App\HistoryProfile;
+use Carbon\Carbon;
 
 class ProfileController extends Controller
 {
@@ -64,7 +66,12 @@ class ProfileController extends Controller
 
     $profile->fill($profile_form)->save();
 
-    return redirect('admin/profile');
+    $history = new HistoryProfile;
+    $history->profile_id = $profile->id;
+    $history->edited_at = Carbon::now();
+    $history->save();
+
+    return redirect('admin/profile/');
     }
     public function delete(Request $request)
     {
--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -23,30 +23,44 @@
                     <div class="form-group row">
                         <label class="col-md-2" for="name">名前</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                            <input type="text" class="form-control" name="name" value="{{ $profile_form->name }}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2" for="gender">性別</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="gender" value="{{ old('gender')}}">
+                            <input type="text" class="form-control" name="gender" value="{{ $profile_form->gender}}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2" for="hobby">何か編集</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="hobby" rows="20">{{ old('hobby') }}</textarea>
+                            <textarea class="form-control" name="hobby" rows="20">{{ $profile_form->hobby }}</textarea>
                         </div>
                     </div>
                     <div class="form-group row">
                        <label class="col-md-2" for="introduction">自己紹介欄</label>
                        <div class="col-md-10">
-                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                            <textarea class="form-control" name="introduction" rows="20">{{ $profile_form->introduction }}</textarea>
                        </div>
                     </div>
+                    <input type="hidden" name="id" value="{{ $profile_form->id }}">
                     {{ csrf_field() }}
                     <input type="submit" class="btn btn-primary" value="新規作成">
                 </form>
+                {{-- 以下を追記　--}}
+                <div class="row mt-5">
+                    <div class="col-md-4 mx-auto">
+                        <h2>編集履歴</h2>
+                        <ul class="list-group">
+                            @if ($profile_form->histories != NULL)
+                                @foreach ($profile_form->histories as $history)
+                                    <li class="list-group-item">{{ $history->edited_at }}</li>
+                                @endforeach
+                            @endif
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
